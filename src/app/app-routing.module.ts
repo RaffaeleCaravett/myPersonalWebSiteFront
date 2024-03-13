a0fb11ead2d0c3c2824d40f3f1f63c5fa64a4217
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-13 17:56:19 +0100

    at this point started about and other components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { AboutComponent } from './about/about.component';
+import { TalkComponent } from './components/talk/talk.component';
 
 const routes: Routes = [
   {
     path:'',
     component:HomeComponent
-  }
+  },
+  {
+    path:'talks',
+    component:TalkComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'contact',
+    component:ContactComponent
+  },
+  {
+    path:'**',
+    component:NotFoundComponent
+  },
 ];
 
 @NgModule({
