41742b95ada31a35e2d758aed71474e5f760a102
RaffaeleCaravett <136345805+RaffaeleCaravett@users.noreply.github.com>
2024-03-13 11:11:43 +0100

    doing home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+windowWidth:number=0
+
+ngOnInit(): void {
+  this.windowWidth=window.innerWidth
+}
+
+@HostListener('window:resize', ['$event'])
+onResize(event:any) {
+this.windowWidth=window.innerWidth
+}
 }
